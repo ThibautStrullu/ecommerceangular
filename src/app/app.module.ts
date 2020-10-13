a5f1f71b894a80f92b391f9c75074fb643dd7f32
Thibaut STRULLU <thibaut.strullu@aurera.fr>
2020-10-13 15:56:42 +0200

    ajout component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from  '@angular/common/http';
+import { ProductsListComponent } from './components/products-list/products-list.component';
+import { ProductsListItemComponent } from './components/products-list-item/products-list-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductsListComponent,
+    ProductsListItemComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
